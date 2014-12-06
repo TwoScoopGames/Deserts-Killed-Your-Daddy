9f65e91706d5c1ee24ca36baf9984ed6168f9c30
Eric Lathrop <eric@ericlathrop.com>
2014-12-05 23:59:12 -0500

    Add player movement.

--- game.js
+++ game.js
@@ -7,25 +7,63 @@ var manifest = require("./manifest.json");
 
 var game = new Splat.Game(canvas, manifest);
 
-function centerText(context, text, offsetX, offsetY) {
-	var w = context.measureText(text).width;
-	var x = offsetX + (canvas.width / 2) - (w / 2) | 0;
-	var y = offsetY | 0;
-	context.fillText(text, x, y);
-}
-
 game.scenes.add("title", new Splat.Scene(canvas, function() {
 	// initialization
-}, function() {
+	this.player = new Splat.Entity(50, 50, 50, 50);
+	this.player.draw = function(context) {
+		context.fillStyle = "red";
+		context.fillRect(this.x, this.y, this.width, this.height);
+	};
+}, function(elapsedMillis) {
 	// simulation
+	var accel = 0.03;
+	var maxV = 0.8;
+	var xPressed = false;
+	var yPressed = false;
+
+	if (game.keyboard.isPressed("w")) {
+		this.player.vy -= accel;
+		xPressed = true;
+	}
+	if (game.keyboard.isPressed("s")) {
+		this.player.vy += accel;
+		xPressed = true;
+	}
+	if (!xPressed) {
+		if (this.player.vy < 0) {
+			this.player.vy = Math.min(this.player.vy + accel, 0);
+		} else if (this.player.vy > 0) {
+			this.player.vy = Math.max(this.player.vy - accel, 0);
+		}
+	}
+
+	this.player.vy = Math.min(maxV, Math.max(-maxV, this.player.vy));
+
+	if (game.keyboard.isPressed("a")) {
+		this.player.vx -= accel;
+		yPressed = true;
+	}
+	if (game.keyboard.isPressed("d")) {
+		this.player.vx += accel;
+		yPressed = true;
+	}
+	if (!yPressed) {
+		if (this.player.vx < 0) {
+			this.player.vx = Math.min(this.player.vx + accel, 0);
+		} else if (this.player.vx > 0) {
+			this.player.vx = Math.max(this.player.vx - accel, 0);
+		}
+	}
+
+	this.player.vx = Math.min(maxV, Math.max(-maxV, this.player.vx));
+
+	this.player.move(elapsedMillis);
 }, function(context) {
 	// draw
-	context.fillStyle = "#092227";
+	context.fillStyle = "#000000";
 	context.fillRect(0, 0, canvas.width, canvas.height);
 
-	context.fillStyle = "#fff";
-	context.font = "25px helvetica";
-	centerText(context, "Blank SplatJS Project", 0, canvas.height / 2 - 13);
+	this.player.draw(context);
 }));
 
 game.scenes.switchTo("loading");
