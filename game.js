fffb0914facc779726cdbb640587911bd49eed33
Eric Lathrop <eric@ericlathrop.com>
2014-12-06 22:13:20 -0500

    Extract move-entity to file.

--- game.js
+++ game.js
@@ -7,62 +7,7 @@ var manifest = require("./manifest.json");
 
 var game = new Splat.Game(canvas, manifest);
 
-function moveEntity(entity, up, down, left, right, accel, maxV) {
-	var xPressed = left || right;
-	var yPressed = up || down;
-
-	if (up) {
-		entity.vy -= accel;
-		if (!xPressed) {
-			entity.direction = "up";
-			entity.sprite = entity.walkUp;
-		}
-	}
-	if (down) {
-		entity.vy += accel;
-		if (!xPressed) {
-			entity.direction = "down";
-			entity.sprite = entity.walkDown;
-		}
-	}
-	if (!yPressed) {
-		if (entity.vy < 0) {
-			entity.vy = Math.min(entity.vy + accel, 0);
-		} else if (entity.vy > 0) {
-			entity.vy = Math.max(entity.vy - accel, 0);
-		}
-	}
-
-	entity.vy = Math.min(maxV, Math.max(-maxV, entity.vy));
-
-	if (left) {
-		entity.vx -= accel;
-		if (!yPressed) {
-			entity.direction = "left";
-			entity.sprite = entity.walkLeft;
-		}
-	}
-	if (right) {
-		entity.vx += accel;
-		if (!yPressed) {
-			entity.direction = "right";
-			entity.sprite = entity.walkRight;
-		}
-	}
-	if (!xPressed) {
-		if (entity.vx < 0) {
-			entity.vx = Math.min(entity.vx + accel, 0);
-		} else if (entity.vx > 0) {
-			entity.vx = Math.max(entity.vx - accel, 0);
-		}
-	}
-
-	entity.vx = Math.min(maxV, Math.max(-maxV, entity.vx));
-
-	if (entity.vx === 0 && entity.vy === 0) {
-		entity.sprite.reset();
-	}
-}
+var moveEntity = require("./lib/move-entity.js");
 
 function movePlayer(player) {
 	moveEntity(player, game.keyboard.isPressed("w"), game.keyboard.isPressed("s"), game.keyboard.isPressed("a"), game.keyboard.isPressed("d"), 0.03, 0.8);
